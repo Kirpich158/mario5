d871b087aeaf928ccca9ada1e8915d87f2cac801
Kirill_Zub <kirillka.zub@gmail.com>
2024-02-08 17:38:50 +0100

    commented coroutine usage, for settings to appear used SetActive() for now

--- Assets/scripts/MainMenuScript.cs
+++ Assets/scripts/MainMenuScript.cs
@@ -3,16 +3,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour {
-    public CanvasGroup settingsCanvas;
+    public CanvasGroup settingsCanvasGroup;
 
     public void OnPlayButton() {
         SceneManager.LoadScene("Lvl1");
-        Debug.Log("play clicked");
     }
 
     public void OnSettingsButton() {
-        Fade(true);
-        Debug.Log("settings clicked");
+        settingsCanvasGroup.gameObject.SetActive(true);
+        // StartCoroutine(Fade(true));
     }
 
     public void OnResolutionChange() {
@@ -20,8 +19,8 @@ public class MainMenuScript : MonoBehaviour {
     }
 
     public void OnSettingsSave() {
-        Fade(false);
-        Debug.Log("settings saved");
+        settingsCanvasGroup.gameObject.SetActive(false);
+        // StartCoroutine(Fade(false));
     }
 
     public void OnExitButton() {
@@ -32,7 +31,7 @@ public class MainMenuScript : MonoBehaviour {
         int denier = isFadingIn ? 1 : -1;
 
         for (float alpha = 1f; alpha >= 0; alpha += 0.1f * denier) {
-            settingsCanvas.alpha = alpha;
+            settingsCanvasGroup.alpha = alpha;
             yield return null;
         }
     }
